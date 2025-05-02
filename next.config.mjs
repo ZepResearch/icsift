b86f4b64661180448dd69b67593fe7ea77bdcd33
ShardulVanage <vsharadul@gmail.com>
2025-05-02 14:33:26 +0530

    force cache remove

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['res.cloudinary.com', 'images.unsplash.com', 'plus.unsplash.com','zep-research.pockethost.io'],
-    },
-    experimental: {
-      serverActions: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'images.unsplash.com',
+      'plus.unsplash.com',
+      'zep-research.pockethost.io',
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Match all routes
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
