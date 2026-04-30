1e95cdc849a9c5ec60d7659cb55c59eb48b37309
ShardulVanage <vsharadul@gmail.com>
2026-04-30 15:06:28 +0530

    feat: add conference details for the 3rd International Conference on Sustainability, Innovation, and Future Technologies

---
+++ src/constants/conference.js
@@ -0,0 +1,11 @@
+export const CONFERENCE = {
+  name: "3rd International Conference on Sustainability, Innovation, and Future Technologies",
+  shortForm: "3rd ICSIFT",
+  year: 2026,
+  date: "August 21st - 22nd, 2026",
+  venue: {
+    location: "Boracay, Philippines",
+    name: "Astoria Current",
+    address: "Sitio Mangayad, Brgy. Manoc Manoc, Station 3, Malay, Boracay Island, 5608 Aklan, Philippines",
+  },
+};
