d4000ce26597804304bef2e72330eb0d283a9740
ShardulVanage <vsharadul@gmail.com>
2026-05-06 09:58:53 +0530

    fix: update SubmissionSuccess component to display CONFERENCE short form and name

--- src/app/(pages)/submission/success/page.jsx
+++ src/app/(pages)/submission/success/page.jsx
@@ -39,7 +39,8 @@ export default function SubmissionSuccess() {
               </span>
             </h1>
             <p className="text-xl text-[#4d724d] mb-8">
-              Thank you for contributing to {CONFERENCE
+              Thank you for contributing to  {CONFERENCE.name}
+            </p>
           </div>
         </div>
       </section>
@@ -64,7 +65,7 @@ export default function SubmissionSuccess() {
 
                 <div className="space-y-6 text-[#4d724d] max-w-2xl mx-auto">
                   <p>
-                    Thank you for submitting your paper to ICSIFT  International Conference on Sustainability,
+                    Thank you for submitting your paper to {CONFERENCE.shortForm}  International Conference on Sustainability,
                     Innovation, and Future Technologies. Your contribution is an important part of advancing research
                     and collaboration in sustainability and innovation.
                   </p>
